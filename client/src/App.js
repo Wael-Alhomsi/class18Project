caa4edb1aa522b3cab647097438c90d53524a3e9
Wael-Alhomsi <waelhom2@gmail.com>
2019-04-05 22:35:30 +0200

    added homework week1

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
+import ListOfHouses from './components/ListOfHouses.js';
+import HouseDetails from './components/HouseDetails.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <ul>
+                    <li>
+                        <Link to="/api">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/api/houses">Houses</Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route exact path="/api" render={() => <div>Home</div>} />
+                    <Route exact path="/api/houses" component={ListOfHouses} />
+                    <Route
+                        exact
+                        path="/api/houses/:id"
+                        component={HouseDetails}
+                    />
+
+                    <Route render={() => <div>404</div>} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/HouseDetails.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+export default class HouseDetails extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            houseDetails: {},
+            error: null,
+            loading: null,
+        };
+    }
+    componentDidMount() {
+        this.setState({ loading: true });
+        fetch('http://localhost:8080/api/houses/' + this.props.match.params.id)
+            .then(data => data.json())
+            .then(details => {
+                if (details.error) {
+                    this.setState({
+                        error: details.error,
+                        loading: null,
+                    });
+                } else {
+                    this.setState({
+                        houseDetails: details[0],
+                        error: null,
+                        loading: null,
+                    });
+                }
+            })
+            .catch(() => this.setState({ error: 'Something went wrong!' }));
+    }
+
+    render() {
+        const { houseDetails, error, loading } = this.state;
+        if (error) {
+            return <div>{error}</div>;
+        }
+        if (loading) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <ul>
+                <li>House id: {houseDetails.id}</li>
+                <li>Price: {houseDetails.price}</li>
+                <li>Description: {houseDetails.description}</li>
+            </ul>
+        );
+    }
+}
